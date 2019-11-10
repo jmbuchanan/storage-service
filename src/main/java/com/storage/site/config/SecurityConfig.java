62373052663c5bca1fc547e408ea938df8eb06c0
matt <matt@email.com>
2019-11-10 10:21:53 -0500

    remove login

--- src/main/java/com/storage/site/config/SecurityConfig.java
+++ src/main/java/com/storage/site/config/SecurityConfig.java
@@ -1,9 +1,8 @@
-package com.storage.site.config;
+/**package com.storage.site.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -19,15 +18,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void  configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/**").permitAll()
+                    .antMatchers("/**").hasRole("USER")
+                    .antMatchers("/admin/**").hasRole("ADMIN")
                     .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
                     .and()
                 .logout()
                     .permitAll();
     }
+
+   @Override
+   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                    .withUser("user")
+                    .password("{noop}password")
+                    .roles("USER")
+                .and()
+                    .withUser("admin")
+                    .password("{noop}password")
+                    .roles("ADMIN", "USER");
+   }
+
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
@@ -40,3 +51,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 }
+ **/
