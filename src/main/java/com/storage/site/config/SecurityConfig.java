ceb659fccc60aeb5dcbd2c62aa09514b5a3ab610
matt <matt@email.com>
2019-11-14 17:48:02 -0500

    update

--- src/main/java/com/storage/site/config/SecurityConfig.java
+++ src/main/java/com/storage/site/config/SecurityConfig.java
@@ -1,14 +1,16 @@
-/**package com.storage.site.config;
+package com.storage.site.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -17,38 +19,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void  configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
+                .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/**").hasRole("USER")
-                    .antMatchers("/admin/**").hasRole("ADMIN")
-                    .and()
-                .formLogin()
-                    .and()
-                .logout()
-                    .permitAll();
+                    .antMatchers("/authenticate").hasRole("USER")
+                .and()
+                    .formLogin().defaultSuccessUrl("/authenticate");
     }
 
    @Override
    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                     .withUser("user")
-                    .password("{noop}password")
+                    .password(passwordEncoder().encode("password"))
                     .roles("USER")
                 .and()
                     .withUser("admin")
-                    .password("{noop}password")
+                    .password(passwordEncoder().encode("password"))
                     .roles("ADMIN", "USER");
    }
 
-
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
- **/
