69bf1a2b0ff385862d00db877060e46ab653fcbe
matt <matt@email.com>
2019-11-26 20:27:14 -0500

    security

--- src/main/java/com/storage/site/repository/UnitRepository.java
+++ src/main/java/com/storage/site/repository/UnitRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StorageUnitRepository extends CrudRepository<Unit, Long> {
+public interface UnitRepository extends CrudRepository<Unit, Long> {
 }
