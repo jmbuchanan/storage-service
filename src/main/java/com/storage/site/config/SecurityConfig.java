1727b96eeebae6e661537ea14ca9fb03f1b5e783
matt <matt@email.com>
2019-12-01 10:56:59 -0500

    added cors

--- src/main/java/com/storage/site/config/SecurityConfig.java
+++ src/main/java/com/storage/site/config/SecurityConfig.java
@@ -25,7 +25,7 @@ import java.util.Arrays;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${cors.origin}")
-    private String corsOrigin;
+    private String[] corsOrigin;
 
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
