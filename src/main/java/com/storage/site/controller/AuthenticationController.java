be107212411b01df6be5c1d15ad6466691fc30a0
matt <matt@email.com>
2020-03-27 18:02:53 -0400

    snapshot

--- src/main/java/com/storage/site/controller/AuthenticationController.java
+++ src/main/java/com/storage/site/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.storage.site.controller;
 import com.storage.site.config.JwtTokenUtil;
 import com.storage.site.repository.CustomerRepository;
 import com.storage.site.service.CustomerService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -11,13 +12,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-public class LoginController {
+public class AuthenticationController {
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
@@ -32,18 +35,16 @@ public class LoginController {
     PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    ResponseEntity<?> createAuthenticationToken(HttpServletRequest request) {
+    ResponseEntity<String> createAuthenticationToken(HttpServletRequest request) {
 
         //Refactor to only query once
 
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        System.out.println(email);
-        System.out.println(password);
         UserDetails user = customerService.loadUserByUsername(email);
 
         if (user == null) {
-            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         String storedPassword = user.getPassword();
@@ -54,10 +55,27 @@ public class LoginController {
             System.out.println("Login controller token generated: " + token);
             HttpHeaders headers = new HttpHeaders();
             headers.add("Set-Cookie", "Authorization=" + token);
-            return new ResponseEntity<String>(headers, HttpStatus.OK);
+            return new ResponseEntity<>(headers, HttpStatus.OK);
         }
 
-        return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 
+    }
+
+    // doesn't do anything right now
+    @GetMapping("/getUserDetails")
+    ResponseEntity<String> getUserRole(HttpServletRequest request) {
+
+        String authorization = null;
+
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies.length > 0) {
+            for (Cookie c : cookies) {
+                if (c.getName() == "Authorization") {
+                    authorization = c.getValue();
+                }
+            }
+        } return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
