9fa399c7e3b5c3f1c17f68282091b4905074d4fd
matt <matt>
2021-01-04 20:36:17 -0500

    fix

--- db/start_db.sh
+++ db/start_db.sh
@@ -4,4 +4,8 @@ if [[ "$(docker images -q local-postgres 2> /dev/null)" == "" ]]; then
   docker build -t local-postgres .
 fi
 
+if [[ "$(docker ps -a -q --filter ancestor=local-postgres --format="{{.ID}}" 2> /dev/null)" != "" ]]; then
+  docker rm $(docker stop $(docker ps -a -q --filter ancestor=local-postgres --format="{{.ID}}"))
+fi
+
 docker run --rm -p 5432:5432 local-postgres
