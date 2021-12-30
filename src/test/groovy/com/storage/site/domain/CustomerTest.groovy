928707641aa312b02fd6b0d30acc8a3aa6a276f4
Matt Buchanan <jmbuchanan1992@gmail.com>
2021-12-29 19:29:54 -0500

    remove customer test

--- src/test/groovy/com/storage/site/domain/CustomerTest.groovy
+++ src/test/groovy/com/storage/site/domain/CustomerTest.groovy
@@ -4,22 +4,4 @@ import spock.lang.Specification
 
 class CustomerTest extends Specification {
 
-    def "customer email is set to lowercase"() {
-        given:
-        def customer = new Customer()
-
-        when:
-        customer.setEmail("COOL_KID@GMAIL.COM")
-
-        then:
-        customer.getEmail() == "cool_kid@gmail.com"
-    }
-
-    def "empty customer has id of 0" () {
-        given:
-        Customer customer = new Customer()
-
-        expect:
-        customer.getId() == 0
-    }
 }
